d83c00e2eaba93a9d5a5669beb971d8dce7fcf8f
sryglkglk <sry.kuliah.unpam@gmail.com>
2026-06-04 20:33:00 +0700

    Update Revisi P15

--- Latihan_P15/cookie1.php
+++ Latihan_P15/cookie1.php
@@ -7,4 +7,5 @@ setcookie("namalengkap", $value2, time() + 3600); // expire in 1 hour
 
 echo "<h1>Ini halaman pengesetan cookie</h1>";
 echo "<h2>Klik <a href='cookie2.php'>di sini</a> untuk pemeriksaan cookies</h2>";
+// <!-- Project 15.1 By ASEP SURYA AGUSTIN - 221011450392  -->
 ?>
